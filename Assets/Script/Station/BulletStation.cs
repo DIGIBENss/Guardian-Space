b206e39071dae02cdac597dd2d2ea4ef0d399c87
Ivan Zalivokhin <i.zalivokhin@devcity-project.ru>
2024-02-22 13:42:25 +0300

    0.046

--- Assets/Script/Station/BulletStation.cs
+++ Assets/Script/Station/BulletStation.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class BulletStation : MonoBehaviour
 {
     public float speed = 10f;
-    public float lifeDuration = 1f;
+    public float lifeDuration = 5f;
 
     private void Start()
     {
-        
+        Destroy(gameObject, lifeDuration);
     }
 
     private void Update()
@@ -17,14 +17,44 @@ public class BulletStation : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
-    private void OnColliderEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.GetComponent<EnemyHealth>() != null)
+        if (other.gameObject.GetComponent<EnemyHealth>() != null)
         {
             print("enemy");
             var enemy = other.gameObject.GetComponent<EnemyHealth>();
-            enemy.TakeDamage(50,0);
-           
+            enemy.TakeDamage(50, 0);
+            Destroy(gameObject);
         }
     }
+
+
+    //public float speed = 10f;
+    //public float lifeDuration = 1f;
+
+    //private Rigidbody rb;
+
+    //private void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //    rb.velocity = transform.forward * speed;
+    //    Destroy(gameObject, lifeDuration);
+    //}
+    ////private void Update()
+    ////{
+    ////    Vector2 movement = new Vector2(transform.forward.x, transform.forward.z);
+    ////    transform.Translate(movement * speed * Time.deltaTime);
+    ////    //transform.Translate(speed * Time.deltaTime * Vector3.forward);
+    ////}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.GetComponent<EnemyHealth>() != null)
+    //    {
+    //        print("enemy");
+    //        var enemy = other.gameObject.GetComponent<EnemyHealth>();
+    //        enemy.TakeDamage(50, 0);
+    //        Destroy(gameObject);
+    //    }
+    //}
 }
