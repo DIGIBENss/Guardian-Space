deea71a2dcaab19f9747ea75d0913d02b044641b
Ivan <igorevih033@gmail.com>
2024-03-30 15:34:30 +0300

    0.068

---
+++ Assets/Script/Menu/MenuManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject MenuUI;
+    [SerializeField] private GameObject Button;
+
+
+    public void Vivod()
+    {
+        if (MenuUI.activeSelf)
+        {
+            MenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            Button.SetActive(true);
+        }
+        else
+        {
+            MenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            Button.SetActive(false);
+        }
+    }
+}
