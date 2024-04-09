e0a73fd909468a7cc806c7d7a685d4b1a2b270a4
Master_Shadows <pozitivmr15@gmail.com>
2024-04-09 21:24:50 +0300

    0.2082

---
+++ Assets/Quitss.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitss : MonoBehaviour
+{
+   public void ExitGame()
+   {
+      Application.Quit();
+   }
+}
